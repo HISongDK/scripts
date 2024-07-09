5c7ade479163b0ef61ce1a263bb7c94776b92023
Song Dekang <1494403731@qq.com>
2024-07-09 10:34:10 +0800

    :sparkles: change url match & selector

--- tamper/copyBvNumber.js
+++ tamper/copyBvNumber.js
@@ -4,7 +4,7 @@
 // @version      2024-07-07
 // @description  try to take over the world!
 // @author       You
-// @match        https://www.bilibili.com/video/BV1TS421R7Ss/?spm_id_from=333.1007.tianma.1-1-1.click&vd_source=5566a14752976921899272d588362b83
+// @match        https://www.bilibili.com/video/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=bilibili.com
 // @grant        none
 // ==/UserScript==
@@ -14,7 +14,7 @@
     window.addEventListener('load', () => {
         setTimeout(() => {
             const title = document.querySelector(
-                '#viewbox_report > div.video-info-title > div.video-info-title-inner.video-info-title-inner-overflow > h1',
+                '#viewbox_report > div.video-info-title > div > h1',
             )
             title.addEventListener('click', () => {
                 const bv = location.pathname.split('/')[2]
